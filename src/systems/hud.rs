a9b6ccf4ae0b69bd9e0fbfb29fd93766f31b71e2
AJV <alexander.valarus@gmail.com>
2023-06-22 23:25:44 -0400

    Items can now be used

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -39,16 +39,14 @@ pub fn hud(ecs: &SubWorld) {
         .iter(ecs)
         .filter(|(_, _, carried)| carried.0 == player)
         .for_each(|(_, name, _)| {
-            draw_batch.print(
-                Point::new(3, y),
-                format!("{} : {}", y-2, &name.0)
-            );
+            draw_batch.print(Point::new(3, y), format!("{} : {}", y - 2, &name.0));
             y += 1;
-        }
-    );
+        });
     if y > 3 {
-        draw_batch.print_color(Point::new(3, 2), "Items carried",
-        ColorPair::new(YELLOW, BLACK)
+        draw_batch.print_color(
+            Point::new(3, 2),
+            "Items carried",
+            ColorPair::new(YELLOW, BLACK),
         );
     }
     draw_batch.submit(10000).expect("Batch error");
