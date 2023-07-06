0521bc94e5884c0ea4a098657a8dec183606a971
AJV <alexander.valarus@gmail.com>
2023-07-05 23:23:49 -0400

    Added additional weapons. Monsters now do variable damage per hit.

--- src/spawner/mod.rs
+++ src/spawner/mod.rs
@@ -8,7 +8,7 @@ pub fn spawn_level(
     ecs: &mut World,
     rng: &mut RandomNumberGenerator,
     level: usize,
-    spawn_points: &[Point]
+    spawn_points: &[Point],
 ) {
     let template = Templates::load();
     template.spawn_entities(ecs, rng, level, spawn_points);
@@ -27,6 +27,7 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             max: 10,
         },
         FieldOfView::new(8),
+        Damage(1),
     ));
 }
 
@@ -42,4 +43,3 @@ pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
         Name("Amulet of Yala".to_string()),
     ));
 }
-
--- src/spawner/template.rs
+++ src/spawner/template.rs
@@ -1,9 +1,9 @@
 use crate::prelude::*;
-use serde::Deserialize;
-use ron::de::from_reader;
-use std::fs::File;
-use std::collections::HashSet;
 use legion::systems::CommandBuffer;
+use ron::de::from_reader;
+use serde::Deserialize;
+use std::collections::HashSet;
+use std::fs::File;
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct Template {
@@ -14,6 +14,7 @@ pub struct Template {
     pub glyph: char,
     pub provides: Option<Vec<(String, i32)>>,
     pub hp: Option<i32>,
+    pub base_damage: Option<i32>,
 }
 
 #[derive(Clone, Deserialize, Debug, PartialEq)]
@@ -24,12 +25,11 @@ pub enum EntityType {
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct Templates {
-    pub entities: Vec<Template>
+    pub entities: Vec<Template>,
 }
 impl Templates {
     pub fn load() -> Self {
-        let file = File::open("resources/template.ron")
-            .expect("Failed to open file");
+        let file = File::open("resources/template.ron").expect("Failed to open file");
         from_reader(file).expect("Unable to load templates")
     }
     pub fn spawn_entities(
@@ -37,7 +37,7 @@ impl Templates {
         ecs: &mut World,
         rng: &mut RandomNumberGenerator,
         level: usize,
-        spawn_points: &[Point]
+        spawn_points: &[Point],
     ) {
         let mut available_entities = Vec::new();
         self.entities
@@ -49,54 +49,54 @@ impl Templates {
                 }
             });
         let mut commands = CommandBuffer::new(ecs);
-        spawn_points.iter()
-            .for_each(|pt| {
-                if let Some(entity) = rng.random_slice_entry(&available_entities) {
-                    self.spawn_entity(pt, entity, &mut commands);
-                }
-            });
+        spawn_points.iter().for_each(|pt| {
+            if let Some(entity) = rng.random_slice_entry(&available_entities) {
+                self.spawn_entity(pt, entity, &mut commands);
+            }
+        });
         commands.flush(ecs);
     }
 
-    fn spawn_entity(
-        &self,
-        pt: &Point,
-        template: &Template,
-        commands: &mut legion::systems::CommandBuffer
-    ) {
+    fn spawn_entity(&self, pt: &Point, template: &Template, commands: &mut CommandBuffer) {
         let entity = commands.push((
             pt.clone(),
             Render {
                 color: ColorPair::new(WHITE, BLACK),
-                glyph: to_cp437(template.glyph)
+                glyph: to_cp437(template.glyph),
             },
-            Name(template.name.clone())
+            Name(template.name.clone()),
         ));
         match template.entity_type {
-            EntityType::Item => commands.add_component(entity, Item{}),
+            EntityType::Item => commands.add_component(entity, Item {}),
             EntityType::Enemy => {
-                commands.add_component(entity, Enemy{});
+                commands.add_component(entity, Enemy {});
                 commands.add_component(entity, FieldOfView::new(6));
-                commands.add_component(entity, ChasingPlayer{});
-                commands.add_component(entity, Health{
-                    current: template.hp.unwrap(),
-                    max: template.hp.unwrap()
-                });
+                commands.add_component(entity, ChasingPlayer {});
+                commands.add_component(
+                    entity,
+                    Health {
+                        current: template.hp.unwrap(),
+                        max: template.hp.unwrap(),
+                    },
+                );
             }
         }
         if let Some(effects) = &template.provides {
-            effects.iter().for_each(|(provides, n)| {
-                match provides.as_str() {
-                    "Healing" => commands.add_component(entity,
-                                                        ProvidesHealing{amount: *n}),
-                    "MagicMap" => commands.add_component(entity,
-                                                         ProvidesDungeonMap{}),
+            effects
+                .iter()
+                .for_each(|(provides, n)| match provides.as_str() {
+                    "Healing" => commands.add_component(entity, ProvidesHealing { amount: *n }),
+                    "MagicMap" => commands.add_component(entity, ProvidesDungeonMap {}),
                     _ => {
-                        println!("Warning: we don't know how to provide {}",
-                                 provides);
+                        println!("Warning: we don't know how to provide {}", provides);
                     }
-                }
-            });
+                });
+        }
+        if let Some(damage) = &template.base_damage {
+            commands.add_component(entity, Damage(*damage));
+            if template.entity_type == EntityType::Item {
+                commands.add_component(entity, Weapon {});
+            }
         }
     }
 }
